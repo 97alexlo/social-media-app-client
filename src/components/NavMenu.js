578813b04860abf922d3a9cd302fe5c0a6e466b2
alex <7253alex@gmail.com>
2022-04-28 23:59:44 -0700

    fixed import module typo

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/container";
+import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import { Link, useLocation } from "react-router-dom";
